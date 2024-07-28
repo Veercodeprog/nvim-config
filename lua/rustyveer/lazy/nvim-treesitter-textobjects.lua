c6c64654fb1b633b07f6975817b0cf683a73bea7
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-07-28 10:11:43 +0530

    text-objects

--- lua/rustyveer/lazy/nvim-treesitter-textobjects.lua
+++ lua/rustyveer/lazy/nvim-treesitter-textobjects.lua
@@ -26,9 +26,6 @@ return {
 						["]c"] = { query = "@class.outer", desc = "Next class start" },
 						["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
 						["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-
-						-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-						-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
 						["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
 						["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
 					},
@@ -56,35 +53,24 @@ return {
 				},
 				select = {
 					enable = true,
-
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-
+					lookahead = true, -- Automatically jump forward to textobj
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
 						["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
 						["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
 						["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
 						["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
-
-						-- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
 						["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
 						["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
 						["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
 						["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
-
 						["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
 						["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
-
 						["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
 						["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-
 						["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
 						["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-
 						["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
 						["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
-
 						["am"] = {
 							query = "@function.outer",
 							desc = "Select outer part of a method/function definition",
@@ -93,7 +79,6 @@ return {
 							query = "@function.inner",
 							desc = "Select inner part of a method/function definition",
 						},
-
 						["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
 						["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
 					},
@@ -103,11 +88,11 @@ return {
 
 		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
-		-- vim way: ; goes to the direction you were moving.
+		-- Repeat last move
 		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
 		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
-		-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+		-- Repeat built-in moves
 		vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
 		vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
 		vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
