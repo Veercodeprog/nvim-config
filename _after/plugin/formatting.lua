182ac89736efdaad7af8c55c6dd5a682d225251f
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-07-28 10:08:21 +0530

    formatting plugin

--- _after/plugin/formatting.lua
+++ _after/plugin/formatting.lua
@@ -1,7 +1,9 @@
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+	[[
     augroup prettier_config
         autocmd!
         autocmd BufRead,BufNewFile *.js,*.jsx,*.ts,*.tsx,*.json setlocal formatprg=<path_to_prettier_executable>
     augroup END
-]], false)
-
+]],
+	false
+)
